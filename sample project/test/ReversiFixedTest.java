558bb88f7b2d502d84123b8474e395a58a4b1866
s-thora <toma.savkova@gmail.com>
2019-02-19 11:21:51 +0100

    Clean up tests - 1 modifying assert per test

--- sample project/test/ReversiFixedTest.java
+++ sample project/test/ReversiFixedTest.java
@@ -20,16 +20,26 @@ public class ReversiFixedTest {
     }
 
     @Test
-    public void testNotValidMoves() {
+    public void testMoveOnNotEmpty() {
         ReversiFixed game = rev;
-
         assertFalse("move on not empty tile (4,4)", game.move(4,4));
+
         assertEquals("check if didn't change", 0, game.playground[4][4]);
+    }
 
+    @Test
+    public void testMoveOutOfBounds() {
+        ReversiFixed game = rev;
         assertFalse("move on tile out of bounds (0,8)", game.move(0,8));
-        assertFalse("move on tile out of bounds (0,-1)", game.move(0,-1));
 
+        assertFalse("move on tile out of bounds (0,-1)", game.move(0,-1));
+    }
+
+    @Test
+    public void testMoveOnNotAdjacent() {
+        ReversiFixed game = rev;
         assertFalse("not valid move (0,0)", game.move(0,0));
+
         assertEquals("check if didn't change", -1, game.playground[0][0]);
     }
 
@@ -37,6 +47,7 @@ public class ReversiFixedTest {
     public void testFlipRight() {
         ReversiFixed game = rev;
         assertTrue("move to flip", game.move(3, 2));
+
         assertEquals("check if flipped", 1, game.playground[3][3]);
         assertEquals("check if flipped", 1, game.playground[3][2]);
         assertEquals("check on turn", 0, game.onTurn);
@@ -48,6 +59,7 @@ public class ReversiFixedTest {
     public void testFlipUp() {
         ReversiFixed game = rev;
         assertTrue("move to flip", game.move(5, 4));
+
         assertEquals("check if flipped", 1, game.playground[4][4]);
         assertEquals("check if flipped", 1, game.playground[5][4]);
         assertEquals("check on turn", 0, game.onTurn);
@@ -59,6 +71,7 @@ public class ReversiFixedTest {
     public void testFlipLeft() {
         ReversiFixed game = rev;
         assertTrue("move to flip", game.move(4, 5));
+
         assertEquals("check if flipped", 1, game.playground[4][4]);
         assertEquals("check if flipped", 1, game.playground[4][5]);
         assertEquals("check on turn", 0, game.onTurn);
@@ -70,6 +83,7 @@ public class ReversiFixedTest {
     public void testFlipDown() {
         ReversiFixed game = rev;
         assertTrue("move to flip", game.move(2, 3));
+
         assertEquals("check if flipped", 1, game.playground[3][3]);
         assertEquals("check if flipped", 1, game.playground[2][3]);
         assertEquals("check on turn", 0, game.onTurn);
@@ -79,22 +93,9 @@ public class ReversiFixedTest {
 
     @Test
     public void testFlipRightUp() {
-        ReversiFixed game = rev;
-        assertTrue("move to flip", game.move(5, 4));
-        assertEquals("check if flipped", 1, game.playground[4][4]);
-        assertEquals("check if flipped", 1, game.playground[5][4]);
-        assertEquals("check on turn", 0, game.onTurn);
-        assertEquals("O left", 1, game.leftO);
-        assertEquals("X left", 4, game.leftX);
+        int[][] moves = {{5, 4}, {5, 3}, {6, 2}};
+        ReversiFixed game = setMoves(moves);
 
-        assertTrue("move to flip", game.move(5, 3));
-        assertEquals("check if flipped", 0, game.playground[4][3]);
-        assertEquals("check if flipped", 0, game.playground[5][3]);
-        assertEquals("check on turn", 1, game.onTurn);
-        assertEquals("O left", 3, game.leftO);
-        assertEquals("X left", 3, game.leftX);
-
-        assertTrue("move to flip", game.move(6, 2));
         assertEquals("check if flipped", 1, game.playground[5][3]);
         assertEquals("check if flipped", 1, game.playground[6][2]);
         assertEquals("check on turn", 0, game.onTurn);
@@ -104,15 +105,9 @@ public class ReversiFixedTest {
 
     @Test
     public void testFlipLeftUp() {
-        ReversiFixed game = rev;
-        assertTrue("move to flip", game.move(5, 4));
-        assertEquals("check if flipped", 1, game.playground[4][4]);
-        assertEquals("check if flipped", 1, game.playground[5][4]);
-        assertEquals("check on turn", 0, game.onTurn);
-        assertEquals("O left", 1, game.leftO);
-        assertEquals("X left", 4, game.leftX);
+        int[][] moves = {{5, 4}, {5, 5}};
+        ReversiFixed game = setMoves(moves);
 
-        assertTrue("move to flip", game.move(5, 5));
         assertEquals("check if flipped", 0, game.playground[4][4]);
         assertEquals("check if flipped", 0, game.playground[5][5]);
         assertEquals("check on turn", 1, game.onTurn);
@@ -122,22 +117,9 @@ public class ReversiFixedTest {
 
     @Test
     public void testFlipLeftDown() {
-        ReversiFixed game = rev;
-        assertTrue("move to flip", game.move(2, 3));
-        assertEquals("check if flipped", 1, game.playground[3][3]);
-        assertEquals("check if flipped", 1, game.playground[2][3]);
-        assertEquals("check on turn", 0, game.onTurn);
-        assertEquals("O left", 1, game.leftO);
-        assertEquals("X left", 4, game.leftX);
+        int[][] moves = {{2, 3}, {2, 4}, {1, 5}};
+        ReversiFixed game = setMoves(moves);
 
-        assertTrue("move to flip", game.move(2, 4));
-        assertEquals("check if flipped", 0, game.playground[3][4]);
-        assertEquals("check if flipped", 0, game.playground[2][4]);
-        assertEquals("check on turn", 1, game.onTurn);
-        assertEquals("O left", 3, game.leftO);
-        assertEquals("X left", 3, game.leftX);
-
-        assertTrue("move to flip", game.move(1, 5));
         assertEquals("check if flipped", 1, game.playground[2][4]);
         assertEquals("check if flipped", 1, game.playground[1][5]);
         assertEquals("check on turn", 0, game.onTurn);
@@ -147,15 +129,9 @@ public class ReversiFixedTest {
 
     @Test
     public void testFlipRightDown() {
-        ReversiFixed game = rev;
-        assertTrue("move to flip", game.move(2, 3));
-        assertEquals("check if flipped", 1, game.playground[3][3]);
-        assertEquals("check if flipped", 1, game.playground[2][3]);
-        assertEquals("check on turn", 0, game.onTurn);
-        assertEquals("O left", 1, game.leftO);
-        assertEquals("X left", 4, game.leftX);
+        int[][] moves = {{2, 3}, {2, 2}};
+        ReversiFixed game = setMoves(moves);
 
-        assertTrue("move to flip", game.move(2, 2));
         assertEquals("check if flipped", 0, game.playground[3][3]);
         assertEquals("check if flipped", 0, game.playground[2][2]);
         assertEquals("check on turn", 1, game.onTurn);
@@ -165,12 +141,8 @@ public class ReversiFixedTest {
 
     @Test
     public void testDoubleFlip() {
-        ReversiFixed game = rev;
-
-        assertTrue(game.move(2, 3));
-        assertTrue(game.move(2, 2));
-        assertTrue(game.move(3, 2));
-        assertTrue(game.move(2, 4));
+        int[][] moves = {{2, 3}, {2, 2}, {3, 2}, {2, 4}};
+        ReversiFixed game = setMoves(moves);
 
         assertEquals("check if flipped (2,3) correctly",0, game.playground[2][3]);
         assertEquals("check if flipped (3,4) correctly",0, game.playground[3][4]);
@@ -180,47 +152,13 @@ public class ReversiFixedTest {
 
     @Test
     public void testGame1() {
-        ReversiFixed game = rev;
-
-        assertTrue("if the are valid moves", game.areValidMoves());
-
-        assertTrue(game.move(4, 5)); assertTrue(game.move(5, 3)); assertTrue(game.move(3, 2));
-        assertTrue(game.move(2, 3)); assertTrue(game.move(2, 2)); assertTrue(game.move(3, 5));
-        assertTrue(game.move(4, 2)); assertTrue(game.move(2, 1)); assertTrue(game.move(1, 2));
-        assertTrue(game.move(5, 4)); assertTrue(game.move(5, 2)); assertTrue(game.move(3, 1));
-
-        assertEquals("O left", 7, game.leftO);
-        assertEquals("X left", 9, game.leftX);
-
-        assertTrue(game.move(4, 1)); assertTrue(game.move(1, 3)); assertTrue(game.move(2, 4));
-        assertTrue(game.move(5, 0)); assertTrue(game.move(0, 2)); assertTrue(game.move(5, 1));
-        assertTrue(game.move(2, 5)); assertTrue(game.move(5, 5)); assertTrue(game.move(6, 5));
-        assertTrue(game.move(0, 4)); assertTrue(game.move(1, 4)); assertTrue(game.move(0, 5));
-
-        assertEquals("O left", 17, game.leftO);
-        assertEquals("X left", 11, game.leftX);
-
-        assertTrue(game.move(6, 4)); assertTrue(game.move(2, 6)); assertTrue(game.move(6, 2));
-        assertTrue(game.move(3, 6)); assertTrue(game.move(4, 6)); assertTrue(game.move(7, 3));
-        assertTrue(game.move(3, 7)); assertTrue(game.move(6, 3)); assertTrue(game.move(0, 3));
-        assertTrue(game.move(0, 1)); assertTrue(game.move(7, 1)); assertTrue(game.move(7, 2));
-
-        assertEquals("O left", 23, game.leftO);
-        assertEquals("X left", 17, game.leftX);
-
-        assertTrue(game.move(7, 4)); assertTrue(game.move(1, 5)); assertTrue(game.move(2, 7));
-        assertTrue(game.move(5, 6)); assertTrue(game.move(4, 7)); assertTrue(game.move(1, 6));
-        assertTrue(game.move(2, 0)); assertTrue(game.move(7, 5)); assertTrue(game.move(7, 6));
-        assertTrue(game.move(3, 0)); assertTrue(game.move(0, 7)); assertTrue(game.move(1, 0));
-
-        assertTrue("if the are valid moves", game.areValidMoves());
-        assertEquals("O left", 27, game.leftO);
-        assertEquals("X left", 25, game.leftX);
-
-        assertTrue(game.move(0, 6)); assertTrue(game.move(5, 7)); assertTrue(game.move(6, 1));
-        assertTrue(game.move(7, 0)); assertTrue(game.move(6, 0)); assertTrue(game.move(7, 7));
-        assertTrue(game.move(4, 0)); assertTrue(game.move(1, 7)); assertTrue(game.move(0, 0));
-        assertTrue(game.move(1, 1)); assertTrue(game.move(6, 7)); assertTrue(game.move(6, 6));
+        int[][] moves = {{4, 5}, {5, 3}, {3, 2}, {2, 3}, {2, 2}, {3, 5}, {4, 2}, {2, 1}, {1, 2}, {5, 4},
+                {5, 2}, {3, 1}, {4, 1}, {1, 3}, {2, 4}, {5, 0}, {0, 2}, {5, 1}, {2, 5}, {5, 5}, {6, 5},
+                {0, 4}, {1, 4}, {0, 5}, {6, 4}, {2, 6}, {6, 2}, {3, 6}, {4, 6}, {7, 3}, {3, 7}, {6, 3},
+                {0, 3}, {0, 1}, {7, 1}, {7, 2}, {7, 4}, {1, 5}, {2, 7}, {5, 6}, {4, 7}, {1, 6}, {2, 0},
+                {7, 5}, {7, 6}, {3, 0}, {0, 7}, {1, 0}, {0, 6}, {5, 7}, {6, 1}, {7, 0}, {6, 0}, {7, 7},
+                {4, 0}, {1, 7}, {0, 0}, {1, 1}, {6, 7}, {6, 6}};
+        ReversiFixed game = setMoves(moves);
 
         assertFalse("if the are valid moves", game.areValidMoves());
         game.gameOver();
@@ -228,5 +166,15 @@ public class ReversiFixedTest {
         assertEquals("O left", 33, game.leftO);
         assertEquals("X left", 31, game.leftX);
         assertFalse("game is over", game.move(0, 1));
+    }
+
+    private ReversiFixed setMoves(int[][] moves) {
+        ReversiFixed game = new ReversiFixed();
+        for (int[] move  : moves) {
+            int r = move[0];
+            int c = move[1];
+            game.move(r, c);
+        }
+        return game;
     }
 }
