f5494e071d5186c8511170d8cf9b4d0b8afa7f6b
s-thora <toma.savkova@gmail.com>
2019-03-04 15:31:50 +0100

    Update create script

--- server/pg_scripts/create_script.sql
+++ server/pg_scripts/create_script.sql
@@ -1,28 +1,36 @@
-drop table if exists courses;
+drop table if exists courses CASCADE;
 create table courses (
 	id serial primary key,
 	name text,
 	created timestamp
 );
 
-drop table if exists exercises;
+drop table if exists exercise_types CASCADE;
+create table exercise_types (
+	id serial primary key,
+	name text
+);
+
+drop table if exists exercises CASCADE;
 create table exercises (
 	id serial primary key,
 	name text,
 	course_id int references courses on delete cascade,
+	type_id int references exercise_types on delete cascade,
 	created timestamp
 );
 
-drop table if exists source_codes;
+drop table if exists source_codes CASCADE;
 create table source_codes (
 	id serial primary key,
 	filename text,
 	exercise_id int references exercises on delete cascade
 );
 
-drop table if exists hidden_tests;
+drop table if exists hidden_tests CASCADE;
 create table hidden_tests (
 	id serial primary key,
 	filename text,
 	exercise_id int references exercises on delete cascade
 );
+
