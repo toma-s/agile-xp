11f5eabbe7c7c2cfc270380d99d40bd6df6a280a
s-thora <toma.savkova@gmail.com>
2019-03-08 20:05:51 +0100

    Add repository

--- server/src/main/java/com/agilexp/controller/SourceCodeController.java
+++ server/src/main/java/com/agilexp/controller/SourceCodeController.java
@@ -1,6 +1,7 @@
 package com.agilexp.controller;
 
 import com.agilexp.model.SourceCode;
+import com.agilexp.repository.SourceCodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
---
+++ server/src/main/java/com/agilexp/repository/SourceCodeRepository.java
@@ -0,0 +1,7 @@
+package com.agilexp.repository;
+
+import com.agilexp.model.SourceCode;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SourceCodeRepository extends CrudRepository<SourceCode, Long> {
+}
