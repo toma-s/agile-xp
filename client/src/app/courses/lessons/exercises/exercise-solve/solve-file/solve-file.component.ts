442e73c6bbfa6db213be8cc2a86eaef89022b4f1
s-thora <toma.savkova@gmail.com>
2019-03-30 20:43:12 +0100

    Fix toolbar

--- client/src/app/courses/lessons/exercises/exercise-solve/solve-file/solve-file.component.ts
+++ client/src/app/courses/lessons/exercises/exercise-solve/solve-file/solve-file.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ExerciseConfigService } from '../../shared/exercise-config/exercise-config.service';
+import { ExerciseConfig } from '../../shared/exercise-config/exercise-config.model';
 
 @Component({
   selector: 'solve-file',
@@ -12,12 +14,14 @@ export class SolveFileComponent implements OnInit {
   editorOptions = { theme: 'vs', language: 'java'/*, minimap: {'enabled': false}*/ };
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private exerciseConfigService: ExerciseConfigService
   ) { }
 
   ngOnInit() {
     this.updForm();
   }
+
 
   updForm() {
     this.setSolutionFiles();
