59b65cfe972d296e03d78b87da2f22401aa95f2d
s-thora <toma.savkova@gmail.com>
2019-04-12 11:17:29 +0200

    Create new project for tester

---
+++ tester/src/main/java/com/agilexp/AgilexpTesterApplication.java
@@ -0,0 +1,13 @@
+package com.agilexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilexpTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgilexpTesterApplication.class, args);
+    }
+
+}
---
+++ tester/src/test/java/com/agilexp/AgilexpTesterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agilexp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgilexpTesterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
