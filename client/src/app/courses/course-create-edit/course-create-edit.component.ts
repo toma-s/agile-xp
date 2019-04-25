acf231085f4650d8030ad2312c4742085785f5e7
s-thora <toma.savkova@gmail.com>
2019-04-25 19:36:43 +0200

    Fix create+edit component, use form group

--- client/src/app/courses/course-create-edit/course-create-edit.component.ts
+++ client/src/app/courses/course-create-edit/course-create-edit.component.ts
@@ -3,6 +3,7 @@ import { Course } from '../shared/course.model';
 import { CourseService } from '../shared/course.service';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -15,19 +16,20 @@ export class CourseCreateEditComponent implements OnInit {
   course = new Course();
   submitted = false;
   mode: string;
+  courseForm: FormGroup;
 
   constructor(
     private titleService: Title,
     private courseService: CourseService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.setMode();
     this.setTitle();
-    if (this.mode === 'edit') {
-      this.loadCourse();
-    }
+    await this.setCourse();
+    this.createForm();
   }
 
   setMode() {
@@ -44,20 +46,38 @@ export class CourseCreateEditComponent implements OnInit {
     return s.toUpperCase()[0].concat(s.slice(1, s.length));
   }
 
+  async setCourse() {
+    if (this.mode === 'edit') {
+      this.course = await this.loadCourse();
+      console.log(this.course);
+    } else if (this.mode === 'create') {
+      this.course = new Course();
+    }
+  }
 
-  loadCourse() {
+  loadCourse(): Promise<Course> {
     const courseId = Number(this.route.snapshot.params['courseId']);
-    this.courseService.getCourseById(courseId).subscribe(
-      data => {
-        this.course = data;
-      },
-      error => console.log(error)
-    );
+    return new Promise<Course>((resolve, reject) => {
+      this.courseService.getCourseById(courseId).subscribe(
+        data => resolve(data),
+        error => reject(error)
+      );
+    });
+  }
+
+  createForm() {
+    console.log(this.course);
+    this.courseForm = this.fb.group({
+      name: [this.course.name, Validators.required],
+      description: [this.course.description, Validators.required]
+    });
   }
 
 
   submit() {
     console.log(this.course);
+    this.course.name = this.courseForm.get('name').value;
+    this.course.description = this.courseForm.get('description').value;
     if (this.mode === 'create') {
       this.saveCourse();
     } else if (this.mode === 'edit') {
@@ -66,8 +86,7 @@ export class CourseCreateEditComponent implements OnInit {
   }
 
   saveCourse() {
-    this.courseService.createCourse(this.course)
-      .subscribe(
+    this.courseService.createCourse(this.course).subscribe(
         data => {
           console.log(data);
           this.submitted = true;
