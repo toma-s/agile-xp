862faa8bde79a7111943fc668fe70e1a9f87d49a
s-thora <toma.savkova@gmail.com>
2019-04-25 13:07:42 +0200

    Update create course: remove lessons adding

--- client/src/app/courses/course-create/course-create.component.ts
+++ client/src/app/courses/course-create/course-create.component.ts
@@ -14,78 +14,26 @@ import { Title } from '@angular/platform-browser';
 })
 export class CourseCreateComponent implements OnInit {
 
-  public lessonsFormGroup: FormGroup;
-  public lessonsArray: FormArray;
   course = new Course();
-  lessons = new Array<Lesson>();
   submitted = false;
 
   constructor(
     private titleService: Title,
-    private fb: FormBuilder,
-    private courseService: CourseService,
-    private lessonService: LessonService
+    private courseService: CourseService
   ) {}
 
   ngOnInit(): void {
     this.setTitle();
-    this.lessonsFormGroup = this.fb.group({
-      lessons: this.fb.array([this.createLesson()])
-    });
-    this.lessonsArray = this.lessonsFormGroup.get('lessons') as FormArray;
   }
 
   setTitle() {
     this.titleService.setTitle(`Create course | AgileXP`);
   }
 
-  get lessonFormGroup() {
-    return this.lessonsFormGroup.get('lessons') as FormArray;
-  }
-
-  createLesson(): FormGroup {
-    return this.fb.group({
-      lessonName: [null, Validators.compose([Validators.required])],
-      lessonDescription: [null, Validators.compose([Validators.required])]
-    });
-  }
-
-  addLesson() {
-    this.lessonsArray.push(this.createLesson());
-  }
-
-  removeLesson(index) {
-    this.lessonsArray.removeAt(index);
-  }
-
-  getLessonsFormGroup(index): FormGroup {
-    const formGroup = this.lessonsArray.controls[index] as FormGroup;
-    return formGroup;
-  }
-
-  changedLessonType(index) {
-    const validators = Validators.compose([Validators.required]);
-    this.getLessonsFormGroup(index).controls['value'].setValidators(validators);
-    this.getLessonsFormGroup(index).controls['value'].updateValueAndValidity();
-  }
-
   submit() {
-    console.log(this.lessonsFormGroup.value);
     console.log(this.course);
-    this.createLessons();
-    console.log(this.lessons);
     this.saveCourse();
     this.submitted = true;
-  }
-
-  createLessons() {
-    this.lessonsFormGroup.value.lessons.forEach(lessonItem => {
-      console.log(lessonItem);
-      const lesson = new Lesson();
-      lesson.name = lessonItem.lessonName;
-      lesson.description = lessonItem.lessonDescription;
-      this.lessons.push(lesson);
-    });
   }
 
   saveCourse() {
@@ -93,30 +41,13 @@ export class CourseCreateComponent implements OnInit {
       .subscribe(
         data => {
           console.log(data);
-          this.saveLessons(data.id);
-          this.reset();
+          // this.reset();
         },
         error => console.log(error)
       );
   }
 
-  saveLessons(courseId: number) {
-    console.log(courseId);
-    console.log(this.lessons);
-    this.lessons.forEach(lessonObject => {
-      lessonObject.courseId = courseId;
-      this.lessonService.createLesson(lessonObject)
-        .subscribe(
-          data => {
-            console.log(data);
-          },
-          error => console.log(error)
-        );
-    });
-  }
-
-  reset() {
-    this.course = new Course();
-    this.lessons = new Array<Lesson>();
-  }
+  // reset() {
+  //   this.course = new Course();
+  // }
 }
