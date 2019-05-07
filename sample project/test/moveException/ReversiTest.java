2c60ce65ca08b3640468a121e4160b03efd19440
s-thora <toma.savkova@gmail.com>
2019-05-07 21:11:05 +0200

    Move player test to separate file

--- sample project/test/moveException/ReversiTest.java
+++ sample project/test/moveException/ReversiTest.java
@@ -18,16 +18,6 @@ public class ReversiTest {
     public ExpectedException expectedException = ExpectedException.none();
 
 
-    // Player
-
-    @Test
-    public void testPlayerValueOf() {
-        assertEquals("Value of Player B", Player.B, Player.valueOf("B"));
-        assertEquals("Value of Player W", Player.W, Player.valueOf("W"));
-        assertEquals("Value of Player NONE", Player.NONE, Player.valueOf("NONE"));
-    }
-
-
     // readGameConfig
 
     @Test
