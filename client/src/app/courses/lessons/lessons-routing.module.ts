c944edf1177e38441da4777f7755a72861328762
s-thora <toma.savkova@gmail.com>
2019-05-12 23:47:33 +0200

    Fix lessons router

--- client/src/app/courses/lessons/lessons-routing.module.ts
+++ client/src/app/courses/lessons/lessons-routing.module.ts
@@ -3,7 +3,6 @@ import { CoursesListComponent } from '../courses-list/courses-list.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '../../', component: CoursesListComponent},
 ];
 
 @NgModule({
