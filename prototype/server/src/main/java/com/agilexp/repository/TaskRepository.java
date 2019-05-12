94bd463a06d4cf2c2fc4e50f817fe4f9ab391acb
s-thora <toma.savkova@gmail.com>
2019-05-12 10:14:13 +0200

    Remove redundant directories

--- prototype/server/src/main/java/com/agilexp/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agilexp.repository;
-
-import com.agilexp.model.TaskData;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface TaskRepository extends CrudRepository<TaskData, Long> {
-}
