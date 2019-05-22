269f4f578e06b3e1293045044725e6f684fbeca1
s-thora <toma.savkova@gmail.com>
2019-05-22 18:27:50 +0200

    Added updated sql script

--- server/src/main/sql/script.sql
+++ server/src/main/sql/script.sql
@@ -1,95 +1,83 @@
-truncate table
-    courses,
-    lessons,
-    exercise_types,
-    exercises,
-    exercise_content,
-    bugs_number,
-    solutions,
-    solution_content,
-    solution_estimation
-restart identity cascade;
-
 drop table if exists courses CASCADE;
 create table courses (
-	id serial primary key,
-	name text,
-    description text,
-	created timestamp
+                         id serial primary key,
+                         name text,
+                         description text,
+                         created timestamp
 );
 
 drop table if exists lessons CASCADE;
 create table lessons (
-	id serial primary key,
-	name text,
-    description text,
-    index int,
-    created timestamp,
-	course_id int references courses on delete cascade
+                         id serial primary key,
+                         name text,
+                         description text,
+                         index int,
+                         created timestamp,
+                         course_id int references courses on delete cascade
 );
 
 drop table if exists exercise_types CASCADE;
 create table exercise_types (
-	id serial primary key,
-	name text,
-	value text,
-	constraint unique_value unique (name),
-	constraint unique_value unique (value)
+                                id serial primary key,
+                                name text,
+                                value text,
+                                constraint unique_name unique (name),
+                                constraint unique_value unique (value)
 );
 
 drop table if exists exercises CASCADE;
 create table exercises (
-	id serial primary key,
-	name text,
-    description text,
-	index int,
-    created timestamp,
-    solved boolean,
-    type_id int references exercise_types on delete cascade,
-	lesson_id int references lessons on delete cascade
+                           id serial primary key,
+                           name text,
+                           description text,
+                           index int,
+                           created timestamp,
+                           solved boolean,
+                           type_id int references exercise_types on delete cascade,
+                           lesson_id int references lessons on delete cascade
 );
 
 drop table if exists exercise_content cascade;
 create table exercise_content (
-    id serial primary key,
-    filename text,
-    content text,
-    exercise_id int references exercises on delete cascade,
-    exercise_content_type text
+                                  id serial primary key,
+                                  filename text,
+                                  content text,
+                                  exercise_id int references exercises on delete cascade,
+                                  exercise_content_type text
 );
 
 drop table if exists bugs_number cascade;
 create table bugs_number (
-    id serial primary key,
-    exercise_id int references exercises on delete cascade,
-    number int,
-    constraint unique_exercise_id unique (exercise_id)
+                             id serial primary key,
+                             exercise_id int references exercises on delete cascade,
+                             number int,
+                             constraint unique_exercise_id unique (exercise_id)
 );
 
 drop table if exists solutions cascade;
 create table solutions (
-	id serial primary key,
-    created timestamp,
-	exercise_id int references exercises on delete cascade
+                           id serial primary key,
+                           created timestamp,
+                           exercise_id int references exercises on delete cascade
 );
 
 drop table if exists solution_content cascade;
 create table solution_content (
-    id serial primary key,
-    filename text,
-    content text,
-    solution_id int references solutions on delete cascade,
-    solution_content_type text
+                                  id serial primary key,
+                                  filename text,
+                                  content text,
+                                  solution_id int references solutions on delete cascade,
+                                  solution_content_type text
 );
 
 drop table if exists solution_estimation cascade;
 create table solution_estimation (
-	id serial primary key,
-	solution_id int,
-	estimation text,
-	value int,
-    solved boolean,
-    created timestamp
+                                     id serial primary key,
+                                     solution_id int,
+                                     estimation text,
+                                     value int,
+                                     solved boolean,
+                                     created timestamp
 );
 
 INSERT INTO exercise_types (id, name, value)
